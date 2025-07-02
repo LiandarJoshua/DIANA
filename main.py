86b0e8a65987b4edf97517d41b24d56cc101513f
LiandarJoshua <joshuxvro@gmail.com>
2025-07-02 22:52:20 +0400

    api key

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ import sqlite3
 
 
 # Set up API keys
-os.environ["GROQ_API_KEY"] = "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"
+
 api_key = os.getenv("GROQ_API_KEY")
 if not api_key:
     raise EnvironmentError("GROQ_API_KEY environment variable is not set.")
